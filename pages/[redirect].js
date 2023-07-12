cd17a82c5976d169ebd480f19328c214fda59d39
codingshot <45281667+codingshot@users.noreply.github.com>
2023-07-12 16:34:31 -0400

    added links

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -1,17 +1,20 @@
 import { useEffect } from "react";
 
 const RedirectLinks = {
-  "/twitter": "",
-    "/bos": "",
+  "/twitter": "https://twitter.com/VibesProof",
+    "/bos": "https://near.org/near/widget/ProfilePage?accountId=proofofvibes.near",
       "/github": "",
         "/apply": "",
-          "/events": "",
-            "/telegram": "",
+          "/events": "https://neardc.org/telegram",
+            "/telegram": "https://neardc.org/telegram",
               "/tastemakers": "",
-                "/horizon": "",
-                    "/linkedin": "",
+                "/horizon": "https://near.org/nearhorizon.near/widget/Index?tab=project&accountId=proofofvibes.near",
+                    "/linkedin": "https://www.linkedin.com/company/proof-of-vibes/",
                      "/members": "",
-                      "/dao": "",
+                      "/dao": "https://near.org/ndcplug.near/widget/DAO.main?daoId=vibes.sputnik-dao.near",
+                      "/genadrop": "https://genadrop.io/mint/vibe",
+                      "/sbts": "https://i-am-human.app/community-sbts",
+                      "/about": "https://proofofvibes.vercel.app/about/b1db17ba1752463ea6e48e6936cbd839",
 };
 
 const Redirect = () => {
