381e856ab15e5a536c10375622ed252237371136
M-Rb3 <rabea9220@gmail.com>
2023-08-04 00:02:59 +0300

    edit page head element

--- pages/metrics.jsx
+++ pages/metrics.jsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const metrics = () => {
   return (
     <div className="metrics-container">
+      <Head>
+        <title>Proof of Vibes | Metrics</title>
+        <meta
+          name="description"
+          content="The first vibe to earn platform powered by the world's finest tastemakers. "
+        />
+        <meta property="og:title" content="Proof of Vibes | Metrics" />
+        <meta
+          property="og:description"
+          content="The first vibe to earn platform powered by the world's finest tastemakers. "
+        />
+      </Head>
       <div className="metrics-header">Analytics Dashboards</div>
       <div className="metrics-cards">
         <Link href={"/metrics-landing"}>
